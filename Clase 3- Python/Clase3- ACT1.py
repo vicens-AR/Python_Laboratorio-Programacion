e157612a18ad982c025f1ba3bc936a84c4037a23
Vicente Arhex Harwigt <d47797531@instituto.itr.edu.ar>
2024-04-16 08:37:39 -0300

    empezamos las act de la clase 3

---
+++ Clase 3- Python/Clase3- ACT1.py
@@ -0,0 +1,14 @@
+informacion_personal = [
+    {"nombre": "Vicente", "edad": 17},
+    {"nombre": "Negro", "edad": 33},
+    {"nombre": "Joaco", "edad": 28}
+]
+
+
+diccionario1 = informacion_personal[0]
+nomb = diccionario1["nombre"]
+diccionario2 = informacion_personal[1]
+edad = diccionario2["edad"]
+
+print(f"el nombre de la primera persona del diccionario es: {nomb}")
+print(f"la edad de la segunda persona en el diccionario es: {edad}")
