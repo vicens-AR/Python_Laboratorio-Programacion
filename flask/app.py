73e0d6bccd70fd3604bf8526f2fd22d665426130
unknown <d47797531@alumnos.itr.edu.ar>
2024-05-12 14:53:40 -0300

    sigue sin andar el flask

--- flask/app.py
+++ flask/app.py
@@ -3,8 +3,20 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 if __name__ == '__main__':
-  app.run()
+  app.run(debug = True, port=5000)
   
 @app.route('/')
 def casa():
     return render_template('Home.html')
+
+@app.route('/Contacto')
+def Contact():
+    return render_template('Contacto.html')
+
+@app.route('/Curiosidades')
+def Curiosidad():
+    return render_template('Curiosidades.html')
+
+@app.route('/Info_Flask')
+def Info():
+    return render_template('Info_Flask.html')
