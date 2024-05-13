49d37cd05b771ccbf9790f15bd64a4d3a5119fcf
Vicente Arhex Harwigt <d47797531@instituto.itr.edu.ar>
2024-05-13 15:44:23 -0300

    integracion de navbar

--- flask/app.py
+++ flask/app.py
@@ -1,22 +1,22 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
-if __name__ == '__main__':
-  app.run(debug = True, port=5000)
-  
+ 
 @app.route('/')
 def casa():
     return render_template('Home.html')
 
 @app.route('/Contacto')
-def Contact():
+def contacto():
     return render_template('Contacto.html')
 
 @app.route('/Curiosidades')
-def Curiosidad():
+def Curiosidades():
     return render_template('Curiosidades.html')
 
 @app.route('/Info_Flask')
-def Info():
+def Informacion():
     return render_template('Info_Flask.html')
+
+if __name__ == '__main__':
+    app.run(debug = True, port=5000)
